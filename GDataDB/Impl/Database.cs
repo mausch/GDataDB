1114d98e1dc1787aca7fc46d120ed1f62d1f5514
mausch <mauricioscheffer@gmail.com>
2015-08-08 13:15:32 +0100

    Write worksheet headers. Need to test.

--- GDataDB/Impl/Database.cs
+++ GDataDB/Impl/Database.cs
@@ -15,14 +15,15 @@ namespace GDataDB.Impl {
         }
 
         public ITable<T> CreateTable<T>(string name) where T: new() {
-            var length = typeof(T).GetProperties().Length;
+            var fields = new Serializer<T>().GetFields();
+
             var http = client.RequestFactory.CreateRequest();
 
             var request = new XDocument(
                 new XElement(Utils.AtomNs + "entry",
                     new XElement(Utils.AtomNs + "title", name),
                     new XElement(Utils.SpreadsheetsNs + "rowCount", 1),
-                    new XElement(Utils.SpreadsheetsNs + "colCount", length)
+                    new XElement(Utils.SpreadsheetsNs + "colCount", fields.Count())
                     )
                 );
             var response = http.UploadString(worksheetFeed.AbsoluteUri, request.ToString());
@@ -38,7 +39,38 @@ namespace GDataDB.Impl {
                 .Select(e => new Uri(e.Attribute("href").Value))
                 .FirstOrDefault();
 
-            // TODO write headers
+            // write headers
+
+            var key = listFeedUri.Segments[3];
+            key = key.Substring(0, key.Length - 1);
+            var worksheetId = listFeedUri.Segments[4];
+            worksheetId = worksheetId.Substring(0, worksheetId.Length - 1);
+
+            var entries =
+                from field in fields.Select((p, i) => new { p.Name, Index = i })
+                let column = ((char)('A' + field.Index)).ToString() // TODO see what happens beyond Z
+                let cell = column + (field.Index + 1)
+                let id = string.Format("https://spreadsheets.google.com/feeds/cells/{0}/{1}/private/full/R1C{2}", key, worksheetId, field.Index)
+                let edit = id + "/1"
+                select new XElement(Utils.AtomNs + "entry",
+                                new XElement(Utils.BatchNs + "id", cell),
+                                new XElement(Utils.BatchNs + "operation", "update"),
+                                new XElement(Utils.AtomNs + "id", id),
+                                new XElement(Utils.AtomNs + "edit", 
+                                    new XAttribute("rel", "edit"),
+                                    new XAttribute("type", "application/atom+xml"),
+                                    new XAttribute("href", edit)),
+                                new XElement(Utils.SpreadsheetsNs + "cell", 
+                                    new XAttribute("row", 1),
+                                    new XAttribute("col", field.Index),
+                                    new XAttribute("inputValue", field.Name.ToLowerInvariant())));
+
+            var feed = new XDocument(new XElement(Utils.AtomNs + "feed", entries));
+
+            var batchCellUri = string.Format("https://spreadsheets.google.com/feeds/cells/{0}/{1}/private/full/batch", key, worksheetId);
+
+            http = client.RequestFactory.CreateRequest();
+            http.UploadString(batchCellUri, method: "POST", data: feed.ToString());
 
             return new Table<T>(client, listFeedUri: listFeedUri, worksheetUri: editUri);
 
